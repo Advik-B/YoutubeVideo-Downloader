4e497d2496343819c62e0bbdb8b4e34e93faf17b
Advik-B <advik.b@gmail.com>
2021-09-27 15:15:01 +0530

    Update main.py
    Work in progress 😅

--- main.py
+++ main.py
@@ -9,16 +9,4 @@ def cls():
 def clear():
     cls()
 
-def Download_video():
-    cls()
-    button()
-    print('What do you want to search for?\n')
-    query = input('  -: ')
-    link_list = search(query)[0]
-    download_thumb(link_list)
-    im = Image.open("./thumb1.jpg") 
-    im.show()
-    download_AV(link_list)
-    play_vid(r'E:\GIthub repos\YoutubeVideo-Downloader\I Cheated with SECURITY CAMERAS in a Building Competition....mp4')
-
-Download_video()
+#NOTE: Work in progress 😅
