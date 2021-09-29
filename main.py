7a0c92f4a748d5fe1409b1437b690d433731bc9f
Advik-B <advik.b@gmail.com>
2021-09-29 15:36:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
-from functions import *
-from search import *
 from threading import Thread
-from sounds import *
+from platform import system
+from subprocess import call
+from time import sleep
+try:
+    from functions import *
+    from search import *
+    from sounds import *
+except ModuleNotFoundError:
+    from platform import system
+    from subprocess import run
+    call('python -m pip install -r requirements.txt' , shell=True)
+    sleep(10)
+    from functions import *
+    from search import *
+    from sounds import *
+
 from PIL import Image
+#TODO: add support to use optprase module
+
+# -Functions-
 def cls():
+    print(system())
     os.system('cls')
 
 def clear():
