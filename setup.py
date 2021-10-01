b245dc75e33fd73e2b3565529f2d72252e29694d
Advik-B <advik.b@gmail.com>
2021-10-01 16:24:53 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 #NOTE: Only works on windows-10
 
-import os
+from win32com.client import Dispatch
+import os , winshell
 
-desktop = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktopa').replace('\\' , '/')
+desktop = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop').replace('\\' , '/')
 try:
     f = open(f'{desktop}/desktop.ini' , 'r')
     f.close()
 except FileNotFoundError:
     desktop = os.path.join(os.path.join(os.path.expanduser('~')), 'Onedrive\\Desktop').replace('\\' , '/')
 
+print(desktop) #TODO: Remove later
 
+path = desktop.__add__('/Youtube_DL.lnk')  # Path to be saved (shortcut)
+target = r"C:\Users\advik\Desktop\Harlem Shake (Original Army Edition).mp4"  # The shortcut target file or folder
 
-print(desktop)
+shell = Dispatch('WScript.Shell')
+shortcut = shell.CreateShortCut(path)
+shortcut.Targetpath = target
+shortcut.save()
