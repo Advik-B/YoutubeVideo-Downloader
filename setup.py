f5e28193922cc9cce7cb0ab2bb4c3429f54e7a0a
Advik-B <advik.b@gmail.com>
2021-10-01 17:07:48 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,35 @@
 #NOTE: Only works on windows-10
 
 from win32com.client import Dispatch
-import os , winshell
+from clint.textui import progress
+import os
+import requests
 
 
-desktop = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop').replace('\\' , '/')
-try:
-    f = open(f'{desktop}/desktop.ini' , 'r')
-    f.close()
-except FileNotFoundError:
-    desktop = os.path.join(os.path.join(os.path.expanduser('~')), 'Onedrive\\Desktop').replace('\\' , '/')
+def download(url:str , output_folder:str , chunk_size:float=1024):
+    r = requests.get(url, stream=True)
+    path = output_folder.replace('\\' , '/').__add__(f'/{url.split("/")[-1]}')
+    with open(path, 'wb') as f:
+        total_length = int(r.headers.get('content-length'))
+        for chunk in progress.bar(r.iter_content(chunk_size=chunk_size), 'progress: ' ,expected_size=(total_length/chunk_size) + 1): 
+            if chunk:
+                f.write(chunk)
+                f.flush()
 
-print(desktop) #TODO: Remove later
+def Create_Shortcut(path:str , start_in:str , name:str) -> None
+    desktop = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop').replace('\\' , '/')
+    try:
+        f = open(f'{desktop}/desktop.ini' , 'r')
+        f.close()
+    except FileNotFoundError:
+        desktop = os.path.join(os.path.join(os.path.expanduser('~')), 'Onedrive\\Desktop').replace('\\' , '/')
 
-Shortcut_path = desktop.__add__('/Youtube_DL.lnk')  # Path to be saved (shortcut)
-target = r"C:\Users\advik\Desktop\Harlem Shake (Original Army Edition).mp4"  # The shortcut target file or folder
+    print(desktop) #TODO: Remove later
 
-shell = Dispatch('WScript.Shell')
-shortcut = shell.CreateShortCut(Shortcut_path)
-shortcut.Targetpath = target
-shortcut.save()
+    Shortcut_path = desktop.__add__(f'/{name}')  # Path to be saved (shortcut)
+
+    shell = Dispatch('WScript.Shell')
+    shortcut = shell.CreateShortCut(Shortcut_path)
+    shortcut.Targetpath = path
+    shortcut.WorkingDirectory = start_in
+    shortcut.save()
