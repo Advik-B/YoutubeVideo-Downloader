6915ba8bd8aea0e807a9ea5c24a443d863d17bee
Advik-B <advik.b@gmail.com>
2021-10-01 18:09:33 +0530

    .

---
+++ 1.py
@@ -0,0 +1,3 @@
+import os
+local_appdata = os.path.expandvars(r'%LOCALAPPDATA%')
+print(local_appdata)
--- setup.py
+++ setup.py
@@ -3,8 +3,17 @@
 from win32com.client import Dispatch
 from clint.textui import progress
 from sys import stdout
+from subprocess import run
+from time import sleep
+from itertools import cycle
+from threading import Thread
 import os
 import requests
+import tempfile
+import shutil
+
+loading_chars = cycle('/-\|')
+
 
 def download(url:str , output_folder:str , chunk_size:float=1024):
     r = requests.get(url, stream=True)
@@ -34,8 +43,37 @@ def Create_Shortcut(path:str , start_in:str , name:str) -> None:
     shortcut.WorkingDirectory = start_in
     shortcut.save()
 
+def cls():
+    run('cls' , shell=True)
 
-print('Setup is running...')
+def __setup():
+    print('\n\rDownloading app files...')
+    tempdirpath = tempfile.mkdtemp()
+    download('https://github.com/Advik-B/YoutubeVideo-Downloader/raw/virtualenv/YoutubeVideo-Downloader.zip' , tempdirpath)
+    print('\n\nDone!')
+    sleep(4.5)
+    cls()
 
-def setup():
+    local_appdata = os.path.expandvars(r'%LOCALAPPDATA%')
+
+
+
     download('https://github.com/Advik-B/YoutubeVideo-Downloader/raw/virtualenv/YTDL.zip')
+
+
+    
+    shutil.rmtree(tempdirpath , ignore_errors=True)
+
+T = Thread(target=__setup)
+def setup():
+    T.start()
+
+setup()
+# def scrwrite():
+#     while True:
+
+#         stdout.write(f'\rSetup is running {next(loading_chars)}')
+#         sleep(.7)
+
+# S = Thread(target=scrwrite)
+# S.start()
