45a3533629e1644c2f70eff99e3113cd51b63151
Advik <advik.b@gmail.com>
2021-10-12 11:12:35 +0100

    Trying to add gui support

--- main.py
+++ main.py
@@ -1,12 +1,48 @@
-from functions import *
-from search import *
+import os
+
 from threading import Thread
-from sounds import *
+
+from tkinter import *
+
+from tkinter import filedialog, messagebox, ttk
+
+
 from PIL import Image
+
+
+from functions import *
+
+from search import *
+
+from sounds import *
+
+
+
 def cls():
+
+    """Will clear the screen
+    """
+
     os.system('cls')
 
+
 def clear():
-    cls()
+
+    """Will clear the screen
+    """
+
+    os.system('cls')
+    
+
+app = Tk()
+
+app.geometry('800x600+600+200')
+
+
+# Screen-1
+scr1 = ttk.Notebook()
+# Mainloop
+app.mainloop()
+
 
 #NOTE: Work in progress 😅
