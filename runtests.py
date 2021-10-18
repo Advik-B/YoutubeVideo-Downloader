a6efff2c4e9fb9a638b5e0ceda77573cc07e4618
Advik <advik.b@gmail.com>
2021-10-18 08:42:45 +0100

    Include linting in travis CI

--- runtests.py
+++ runtests.py
@@ -1,8 +1,8 @@
 import os
 from fnmatch import fnmatch
-from subprocess import run
+from subprocess import run, TimeoutExpired
+import subprocess as cmd
 from termcolor import cprint
-import _pytest
 
 def listfiles(root, pattern:str='*.*'):
     A = []
@@ -12,7 +12,49 @@ def listfiles(root, pattern:str='*.*'):
                 A.append(os.path.join(path, name))
     return A
 
+cprint('LINTING:', 'blue', attrs=['bold', 'reverse'])
+
+
 files_to_test = listfiles(os.getcwd(), '*.py')
+
+cprint('The following file(s) will be checked:\n', 'green', attrs=['bold', 'underline'])
+
+for file in files_to_test:
+    cprint(f'  {file}' , 'yellow', attrs=['bold'])
+
+
+print()
+print('-'*80)
+print('*'*80)
+print('-'*80)
+print()
+
+
+for file in files_to_test:
+    cprint(f'Checking: {file} 🔎 ', 'green', attrs=['bold', 'underline'])
+    print()
+    o = cmd.getoutput(f'python -m pyflakes {file}')
+    p = cmd.getoutput(f'python -m pylint {file}')
+
+    if not o or not p:
+        cprint('No problems found!','green', attrs=['bold', 'underline'])
+    else:
+        print('pyflakes:\n' , o)
+        print()
+        print()
+        print('*'*80)
+        print()
+        print()
+        print('pylint:\n\t' , p)
+    print()
+    print('-'*80)
+    print()
+
+print('Checking Done!')
+
+cprint('TESTING:', 'blue', attrs=['bold', 'reverse'])
+
+
 fnames = []
 
 for filename in files_to_test:
@@ -32,7 +74,11 @@ for file in files_to_test:
             , 'green', attrs=['bold'])
         print()
         cprint((f'{":"*40}: START :{":"*40}').center(80) ,'cyan', attrs=['reverse', 'bold'])
-        run('python3 -m pytest %s' % file, timeout=60, shell=True)
+        try:
+            run('python3 -m pytest %s' % file, timeout=60, shell=True)
+        except TimeoutExpired as error:
+            cprint(error, color='red', attrs=['bold'])
+
         cprint((f'{":"*40}: END :{":"*40}').center(80) ,'cyan', attrs=['reverse', 'bold'])
         print('\n'*4)
 
