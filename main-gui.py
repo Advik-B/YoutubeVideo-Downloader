47b9e19cc2fdeaad1000e91b71524466fd7b837a
Advik <advik.b@gmail.com>
2021-10-19 06:01:03 +0100

    Update main-gui.py

--- main-gui.py
+++ main-gui.py
@@ -1,22 +1,14 @@
 import os
-
 from threading import Thread
-
 from tkinter import *
-
 from tkinter import filedialog, messagebox, ttk
-
+from ttkthemes import themed_tk as tk
 
 from PIL import Image
 
-
 from functions import *
-
 from search import *
-
-from sounds import *
-
-
+# from sounds import *
 
 def cls():
 
@@ -24,7 +16,6 @@ def cls():
     """
 
     os.system('cls')
-
 
 def clear():
 
@@ -34,15 +25,24 @@ def clear():
     os.system('cls')
     
 
-app = Tk()
+app = tk.ThemedTk()
+screen_width = int(app.winfo_screenwidth())
+screen_height = int(app.winfo_screenheight())
 
-app.geometry('800x600+600+200')
+app_height = int(screen_height * .6)
+app_width = int(screen_width * .5)
+
+app_gm_1 = int(screen_width * .25)
+app_gm_2 = int(screen_height * .1)
+del screen_width
+del screen_height
+
+app.geometry(f'{app_width}x{app_height}+{app_gm_1}+{app_gm_2}') # You can adjust it later
 
 
-# Screen-1
-scr1 = ttk.Notebook()
-# Mainloop
-app.mainloop()
+if __name__ == '__main__':
+    # Mainloop
+    app.mainloop()
 
 
 #NOTE: Work in progress 😅
