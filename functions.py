8361fffd411e0bbb5e7eace618a6dbfbe697f1dd
Advik <advik.b@gmail.com>
2021-10-19 06:00:58 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,5 +1,8 @@
 from pafy import new
-from moviepy.editor import *
+try:
+	from moviepy.editor import *
+except ModuleNotFoundError as e: print(e)
+
 import os
 
 def download_aud(link:str) -> None:
@@ -46,41 +49,44 @@ def getthumb(link:str) -> bytes:
 	r=requests.get(thumb ,allow_redirects=True , timeout=5)
 	return r.content
 
-def play_vid(path:str, name:str=None, fps:int=60 ) -> None:
-    """
-    Play video:
-    -
-    - Will play the video with the given path
+try:	
+	def play_vid(path:str, name:str=None, fps:int=60 ) -> None:
+			"""
+			Play video:
+			-
+			- Will play the video with the given path
 
-    Note:
-    -
-    - The windows title is by default set to the filename:\n
-        ie:\n
-        if the file name is `football.mp4`;\n
-        then:\n
-            the window title will be `football`\n
-    
-    - This function will also try to play the video in 60 FPS ( Frames-Per-Second )\n
+			Note:
+			-
+			- The windows title is by default set to the filename:\n
+				ie:\n
+				if the file name is `football.mp4`;\n
+				then:\n
+					the window title will be `football`\n
+			
+			- This function will also try to play the video in 60 FPS ( Frames-Per-Second )\n
 
-    Optional:
-    -
-    - You can change the window title with `name` parameter
-    - You can change the preffered FPS with `fps` parameter
+			Optional:
+			-
+			- You can change the window title with `name` parameter
+			- You can change the preffered FPS with `fps` parameter
 
 
-    """
-    import pygame
-    _path = path.replace('\\' , '/')
-    __name = _path.split('/')[-1]
-    _name = __name.split('.')[0]
+			"""
+			import pygame
+			_path = path.replace('\\' , '/')
+			__name = _path.split('/')[-1]
+			_name = __name.split('.')[0]
 
-    if name != None:
-        pygame.display.set_caption(name)
-    else:
-        pygame.display.set_caption(_name)
-    
-    clip = VideoFileClip(path)
-    clip.set_fps(fps)
-    clip.preview()
-    clip.close()
-    pygame.quit()
+			if name != None:
+				pygame.display.set_caption(name)
+			else:
+				pygame.display.set_caption(_name)
+			
+			clip = VideoFileClip(path)
+			clip.set_fps(fps)
+			clip.preview()
+			clip.close()
+			pygame.quit()
+except:
+    pass
